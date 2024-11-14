21ed0ecdd2d8f7ab0dde73135fe26f97f4cf84fa
vishal <sarasani.vishalreddy@egovernments.org>
2024-11-14 15:25:21 +0530

    localisation db lock fix changes

--- apps/health_campaign_field_worker_app/lib/data/repositories/local/localization.dart
+++ apps/health_campaign_field_worker_app/lib/data/repositories/local/localization.dart
@@ -10,67 +10,69 @@ import '../../local_store/no_sql/schema/localization.dart';
 class LocalizationLocalRepository {
   FutureOr<List<Localization>> returnLocalizationFromSQL(
       LocalSqlDataStore sql) async {
-    final selectQuery = sql.select(sql.localization).join([]);
+    return retryLocalCallOperation(() async {
+      final selectQuery = sql.select(sql.localization).join([]);
 
-    // List to hold the AND conditions
-    final andConditions = <Expression<bool>>[];
+      // List to hold the AND conditions
+      final andConditions = <Expression<bool>>[];
 
-    // Add condition for locale if provided
-    if (LocalizationParams().locale != null) {
-      final localeString = '${LocalizationParams().locale!}';
-      andConditions.add(sql.localization.locale.equals(localeString));
-    }
-
-    // Add conditions for modules and codes
-    if (LocalizationParams().module != null &&
-        LocalizationParams().module!.isNotEmpty) {
-      final moduleToExclude = LocalizationParams().module!;
-
-      if (LocalizationParams().exclude == true) {
-        // Exclude modules but include records where the code matches
-        final moduleCondition =
-            sql.localization.module.isIn(moduleToExclude.toList()).not();
-        final codeCondition = LocalizationParams().code != null &&
-                LocalizationParams().code!.isNotEmpty
-            ? sql.localization.code.isIn(LocalizationParams().code!.toList())
-            : const Constant(false); // True if no code filter
-
-        // Combine conditions: exclude module unless code matches
-        andConditions.add(buildAnd([moduleCondition | codeCondition]));
-      } else {
-        // Include specified modules and optionally filter by code
-        final moduleCondition =
-            sql.localization.module.isIn(moduleToExclude.toList());
-        final codeCondition = LocalizationParams().code != null &&
-                LocalizationParams().code!.isNotEmpty
-            ? sql.localization.code.isIn(LocalizationParams().code!.toList())
-            : const Constant(false);
-
-        // Combine conditions: module matches and optionally code filter
-        andConditions.add(buildAnd([moduleCondition | codeCondition]));
+      // Add condition for locale if provided
+      if (LocalizationParams().locale != null) {
+        final localeString = '${LocalizationParams().locale!}';
+        andConditions.add(sql.localization.locale.equals(localeString));
       }
-    } else if (LocalizationParams().code != null &&
-        LocalizationParams().code!.isNotEmpty) {
-      // If no module filter, just apply code filter
-      andConditions
-          .add(sql.localization.code.isIn(LocalizationParams().code!.toList()));
-    }
 
-    // Apply the combined conditions to the query
-    if (andConditions.isNotEmpty) {
-      selectQuery.where(buildAnd(andConditions));
-    }
+      // Add conditions for modules and codes
+      if (LocalizationParams().module != null &&
+          LocalizationParams().module!.isNotEmpty) {
+        final moduleToExclude = LocalizationParams().module!;
 
-    final result = await selectQuery.get();
+        if (LocalizationParams().exclude == true) {
+          // Exclude modules but include records where the code matches
+          final moduleCondition =
+              sql.localization.module.isIn(moduleToExclude.toList()).not();
+          final codeCondition = LocalizationParams().code != null &&
+                  LocalizationParams().code!.isNotEmpty
+              ? sql.localization.code.isIn(LocalizationParams().code!.toList())
+              : const Constant(false); // True if no code filter
 
-    return result.map((row) {
-      final data = row.readTableOrNull(sql.localization);
+          // Combine conditions: exclude module unless code matches
+          andConditions.add(buildAnd([moduleCondition | codeCondition]));
+        } else {
+          // Include specified modules and optionally filter by code
+          final moduleCondition =
+              sql.localization.module.isIn(moduleToExclude.toList());
+          final codeCondition = LocalizationParams().code != null &&
+                  LocalizationParams().code!.isNotEmpty
+              ? sql.localization.code.isIn(LocalizationParams().code!.toList())
+              : const Constant(false);
 
-      return Localization()
-        ..code = data!.code
-        ..locale = data.locale
-        ..module = data.module
-        ..message = data.message;
-    }).toList();
+          // Combine conditions: module matches and optionally code filter
+          andConditions.add(buildAnd([moduleCondition | codeCondition]));
+        }
+      } else if (LocalizationParams().code != null &&
+          LocalizationParams().code!.isNotEmpty) {
+        // If no module filter, just apply code filter
+        andConditions.add(
+            sql.localization.code.isIn(LocalizationParams().code!.toList()));
+      }
+
+      // Apply the combined conditions to the query
+      if (andConditions.isNotEmpty) {
+        selectQuery.where(buildAnd(andConditions));
+      }
+
+      final result = await selectQuery.get();
+
+      return result.map((row) {
+        final data = row.readTableOrNull(sql.localization);
+
+        return Localization()
+          ..code = data!.code
+          ..locale = data.locale
+          ..module = data.module
+          ..message = data.message;
+      }).toList();
+    });
   }
 }
