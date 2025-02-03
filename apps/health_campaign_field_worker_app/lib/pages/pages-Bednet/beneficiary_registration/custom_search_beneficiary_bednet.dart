4d6f4a4adb1d6e7c6a148f4f146218c16593ee06
amitverma-egov <amit.verma@egovernments.org>
2025-02-03 18:27:44 +0530

    remove role check from filter on search screen

--- apps/health_campaign_field_worker_app/lib/pages/pages-Bednet/beneficiary_registration/custom_search_beneficiary_bednet.dart
+++ apps/health_campaign_field_worker_app/lib/pages/pages-Bednet/beneficiary_registration/custom_search_beneficiary_bednet.dart
@@ -137,20 +137,19 @@ class _CustomSearchBeneficiaryBednetPageState
                                   }
                                 },
                               ),
-                              if (context.isRegistrar)
-                                Align(
-                                  alignment: Alignment.topLeft,
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.only(left: kPadding),
-                                    child: DigitIconButton(
-                                      textDirection: TextDirection.rtl,
-                                      iconText: getFilterIconNLabel()['label'],
-                                      icon: getFilterIconNLabel()['icon'],
-                                      onPressed: () => showFilterDialog(),
-                                    ),
+                              Align(
+                                alignment: Alignment.topLeft,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: kPadding),
+                                  child: DigitIconButton(
+                                    textDirection: TextDirection.rtl,
+                                    iconText: getFilterIconNLabel()['label'],
+                                    icon: getFilterIconNLabel()['icon'],
+                                    onPressed: () => showFilterDialog(),
                                   ),
                                 ),
+                              ),
                               selectedFilters.isNotEmpty
                                   ? Align(
                                       alignment: Alignment.topLeft,
