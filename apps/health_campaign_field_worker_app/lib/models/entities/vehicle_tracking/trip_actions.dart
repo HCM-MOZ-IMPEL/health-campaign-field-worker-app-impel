b44a5fcd04a476b976e6b3f429941429d694814e
amitverma-egov <amit.verma@egovernments.org>
2025-07-04 12:38:22 +0530

    hddf-2892 , enum import corrected

--- apps/health_campaign_field_worker_app/lib/models/entities/vehicle_tracking/trip_actions.dart
+++ apps/health_campaign_field_worker_app/lib/models/entities/vehicle_tracking/trip_actions.dart
@@ -1,5 +1,7 @@
 import 'package:dart_mappable/dart_mappable.dart';
 
+part 'trip_actions.mapper.dart';
+
 @MappableEnum(caseStyle: CaseStyle.upperCase)
 enum TripActions {
   @MappableValue("start")
