c18ce74636540cf416797c8315f8f95eacf1f240
ritikegov <ritik.kumar@egovernments.org>
2025-07-07 14:25:29 +0530

    add localisation key for BENEFICIARY_ID

--- apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
+++ apps/health_campaign_field_worker_app/lib/utils/i18_key_constants.dart
@@ -1000,6 +1000,7 @@ class SearchBeneficiary {
   String get noOfHouseholdsRegistered => 'NO_OF_HOUSEHOLDS_REGISTERED';
 
   String get noOfResourcesDelivered => 'NO_OF_RESOURCES_DELIVERED';
+  String get beneficiaryId => 'BENEFICIARY_ID';
 
   String get beneficiarySearchHintText => 'BENEFICIARY_SEARCH_HINT_TEXT';
 
