a750f4609595ab9a25cfbdd0404c6727ec2273dd
amitverma-egov <amit.verma@egovernments.org>
2025-07-09 11:53:53 +0530

    version upgrade 1.7

--- apps/health_campaign_field_worker_app/lib/blocs/project/project.dart
+++ apps/health_campaign_field_worker_app/lib/blocs/project/project.dart
@@ -21,6 +21,7 @@ import '../../../models/app_config/app_config_model.dart' as app_configuration;
 import '../../data/local_store/no_sql/schema/app_configuration.dart';
 import '../../data/local_store/no_sql/schema/row_versions.dart';
 import '../../data/local_store/secure_store/secure_store.dart';
+import '../../data/repositories/remote/bandwidth_check.dart';
 import '../../data/repositories/remote/mdms.dart';
 import '../../data/local_store/no_sql/schema/service_registry.dart';
 
@@ -108,10 +109,13 @@ class ProjectBloc extends Bloc<ProjectEvent, ProjectState> {
 
   final DashboardRemoteRepository dashboardRemoteRepository;
 
+  final BandwidthCheckRepository bandwidthCheckRepository;
+
   BuildContext context;
 
   ProjectBloc({
     LocalSecureStore? localSecureStore,
+    required this.bandwidthCheckRepository,
     required this.projectStaffRemoteRepository,
     required this.projectRemoteRepository,
     required this.projectStaffLocalRepository,
