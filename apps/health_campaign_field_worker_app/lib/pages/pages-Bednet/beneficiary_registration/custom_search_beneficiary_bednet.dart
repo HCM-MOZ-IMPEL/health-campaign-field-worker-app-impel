081e68023091183741e45c842365158a2f94ca4b
amitverma-egov <amit.verma@egovernments.org>
2025-07-11 13:24:40 +0530

    hddf- version upgrade set correct householdType in models

--- apps/health_campaign_field_worker_app/lib/pages/pages-Bednet/beneficiary_registration/custom_search_beneficiary_bednet.dart
+++ apps/health_campaign_field_worker_app/lib/pages/pages-Bednet/beneficiary_registration/custom_search_beneficiary_bednet.dart
@@ -561,31 +561,33 @@ class _CustomSearchBeneficiaryBednetPageState
           limit: isPagination
               ? blocWrapper.houseHoldGlobalSearchBloc.state.limit
               : limit,
+          householdType: RegistrationDeliverySingleton().householdType,
         )));
       }
     } else {
       if (isProximityEnabled ||
           selectedFilters.isNotEmpty ||
           searchController.text.isNotEmpty) {
-        blocWrapper.houseHoldGlobalSearchBloc
-            .add(SearchHouseholdsEvent.houseHoldGlobalSearch(
+        blocWrapper.houseHoldGlobalSearchBloc.add(
+            SearchHouseholdsEvent.houseHoldGlobalSearch(
                 globalSearchParams: GlobalSearchParameters(
-          projectId: RegistrationDeliverySingleton().projectId,
-          isProximityEnabled: isProximityEnabled,
-          latitude: lat,
-          longitude: long,
-          maxRadius: RegistrationDeliverySingleton().maxRadius,
-          nameSearch: searchController.text.trim().length > 2
-              ? searchController.text.trim()
-              : blocWrapper.searchHouseholdsBloc.state.searchQuery,
-          filter: selectedFilters,
-          offset: isPagination
-              ? blocWrapper.houseHoldGlobalSearchBloc.state.offset
-              : offset,
-          limit: isPagination
-              ? blocWrapper.houseHoldGlobalSearchBloc.state.limit
-              : limit,
-        )));
+                    projectId: RegistrationDeliverySingleton().projectId,
+                    isProximityEnabled: isProximityEnabled,
+                    latitude: lat,
+                    longitude: long,
+                    maxRadius: RegistrationDeliverySingleton().maxRadius,
+                    nameSearch: searchController.text.trim().length > 2
+                        ? searchController.text.trim()
+                        : blocWrapper.searchHouseholdsBloc.state.searchQuery,
+                    filter: selectedFilters,
+                    offset: isPagination
+                        ? blocWrapper.houseHoldGlobalSearchBloc.state.offset
+                        : offset,
+                    limit: isPagination
+                        ? blocWrapper.houseHoldGlobalSearchBloc.state.limit
+                        : limit,
+                    householdType:
+                        RegistrationDeliverySingleton().householdType)));
       }
     }
   }
