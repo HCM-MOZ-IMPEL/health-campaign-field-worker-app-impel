1b9cf1e18143e7e62e52078059072855da368a81
ritikegov <ritik.kumar@egovernments.org>
2025-07-22 15:25:41 +0530

    change the mobility assessment page and create task

--- apps/health_campaign_field_worker_app/lib/pages/pages-SMC/beneficiary/conduct_mobility_control_assessment.dart
+++ apps/health_campaign_field_worker_app/lib/pages/pages-SMC/beneficiary/conduct_mobility_control_assessment.dart
@@ -1,160 +1,243 @@
+import 'package:digit_ui_components/widgets/atoms/digit_checkbox.dart';
+import 'package:registration_delivery/blocs/household_overview/household_overview.dart';
+import 'package:registration_delivery/router/registration_delivery_router.gm.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:digit_ui_components/digit_components.dart';
-import 'package:digit_ui_components/theme/digit_extended_theme.dart';
-import 'package:digit_ui_components/widgets/molecules/digit_card.dart';
+import 'package:digit_components/digit_components.dart';
+import 'package:digit_components/widgets/atoms/digit_toaster.dart';
+import 'package:digit_components/widgets/atoms/text_block.dart';
+import 'package:digit_data_model/data_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:registration_delivery/blocs/delivery_intervention/deliver_intervention.dart';
+import 'package:registration_delivery/models/entities/additional_fields_type.dart';
+import 'package:registration_delivery/models/entities/status.dart';
+import 'package:registration_delivery/models/entities/task.dart';
+import 'package:registration_delivery/utils/utils.dart';
 import 'package:registration_delivery/utils/i18_key_constants.dart' as i18;
-import '../../../utils/utils_smc/i18_key_constants.dart' as i18_local_smc;
+import '../../../utils/utils_smc/i18_key_constants.dart' as i18_local;
 import 'package:registration_delivery/widgets/back_navigation_help_header.dart';
-import 'package:registration_delivery/widgets/localized.dart';
-import '../../../router/app_router.dart';
+import '../../../widgets/localized.dart';
+import '../../../utils/constants.dart';
 
 @RoutePage()
 class ConductMobilityControlAssessmentPage extends LocalizedStatefulWidget {
-  const ConductMobilityControlAssessmentPage();
+  const ConductMobilityControlAssessmentPage({
+    super.key,
+    super.appLocalizations,
+  });
 
   @override
   State<ConductMobilityControlAssessmentPage> createState() =>
-      ConductMobilityControlAssessmentPageState();
+      _ConductMobilityControlAssessmentPageState();
 }
 
-class ConductMobilityControlAssessmentPageState
+class _ConductMobilityControlAssessmentPageState
     extends LocalizedState<ConductMobilityControlAssessmentPage> {
-  bool _checkbox1 = false;
-  bool _checkbox2 = false;
-  bool _checkbox3 = false;
-  bool _checkbox4 = false;
-  bool _checkbox5 = false;
-  bool _checkbox6 = false;
-  bool _checkbox7 = false;
-  bool _checkbox8 = false;
+  final List<Map<String, String>> controlAssessmentQuestions = [
+    {"key": "checkbox1", "value": "checkbox_1"},
+    {"key": "checkbox2", "value": "checkbox_2"},
+    {"key": "checkbox3", "value": "checkbox_3"},
+    {"key": "checkbox4", "value": "checkbox_4"},
+    {"key": "checkbox5", "value": "checkbox_5"},
+  ];
+
+  final Map<String, bool> checkboxStates = {};
 
   final clickedStatus = ValueNotifier<bool>(false);
 
   @override
-  void dispose() {
-    clickedStatus.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    for (var item in controlAssessmentQuestions) {
+      checkboxStates[item['value']!] = false;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final textTheme = theme.digitTextTheme(context);
 
     return Scaffold(
-      body: SafeArea(
-        child: ScrollableContent(
-          enableFixedDigitButton: true,
-          header: const Column(
-            children: [
-              BackNavigationHelpHeaderWidget(
-                showBackNavigation: false,
-                showHelp: false,
-              ),
-            ],
-          ),
-          footer: DigitCard(
-            margin: const EdgeInsets.only(top: spacer2),
-            children: [
-              ValueListenableBuilder(
-                valueListenable: clickedStatus,
-                builder: (context, bool isClicked, _) {
-                  return DigitButton(
-                    label: localizations.translate(i18.common.coreCommonNext),
-                    type: DigitButtonType.primary,
-                    size: DigitButtonSize.large,
-                    mainAxisSize: MainAxisSize.max,
-                    // isDisabled:
-                    //     isClicked || !(_checkbox1 && _checkbox2 && _checkbox3),
-                    onPressed: () {},
-                  );
-                },
-              ),
-            ],
-          ),
-          children: [
-            DigitCard(
-              margin: const EdgeInsets.only(top: spacer2, bottom: spacer2),
-              children: [
-                Text(
-                  localizations.translate(
-                    i18.deliverIntervention.wasTheDoseAdministered,
+      body: BlocConsumer<DeliverInterventionBloc, DeliverInterventionState>(
+        listener: (context, deliverState) {
+          final router = context.router;
+        },
+        builder: (context, deliverState) {
+          return ReactiveFormBuilder(
+            form: () => buildForm(),
+            builder: (context, form, child) {
+              return ScrollableContent(
+                header: const Column(children: [
+                  BackNavigationHelpHeaderWidget(
+                    showHelp: false,
+                    showcaseButton: null,
                   ),
-                  style: textTheme.headingXl,
-                ),
-                Column(
-                  children: List.generate(8, (index) {
-                    final checkboxValues = [
-                      _checkbox1,
-                      _checkbox2,
-                      _checkbox3,
-                      _checkbox4,
-                      _checkbox5,
-                      _checkbox6,
-                      _checkbox7,
-                      _checkbox8,
-                    ];
-                    final checkboxSetters = [
-                      (bool val) => _checkbox1 = val,
-                      (bool val) => _checkbox2 = val,
-                      (bool val) => _checkbox3 = val,
-                      (bool val) => _checkbox4 = val,
-                      (bool val) => _checkbox5 = val,
-                      (bool val) => _checkbox6 = val,
-                      (bool val) => _checkbox7 = val,
-                      (bool val) => _checkbox8 = val,
-                    ];
-                    final textKeys = [
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText1,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText2,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText3,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText4,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText5,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText6,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText7,
-                      i18_local_smc
-                          .deliverIntervention.conductMobilityAssessmentText8,
-                    ];
-                    return ListTile(
-                      title: Text(
-                        localizations.translate(textKeys[index]),
-                      ),
-                      leading: SizedBox(
-                        width: 24,
-                        height: 24,
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: DigitCheckbox(
-                            value: checkboxValues[index],
-                            onChanged: (bool? value) {
-                              setState(() {
-                                checkboxSetters[index](value ?? false);
-                              });
-                            },
+                ]),
+                enableFixedButton: true,
+                footer: DigitCard(
+                  margin: const EdgeInsets.fromLTRB(0, kPadding, 0, 0),
+                  padding: const EdgeInsets.fromLTRB(kPadding, 0, kPadding, 0),
+                  child: ValueListenableBuilder(
+                    valueListenable: clickedStatus,
+                    builder: (context, bool isClicked, _) {
+                      return DigitElevatedButton(
+                        onPressed: isClicked
+                            ? null
+                            : () async {
+                                if (context.mounted) {
+                                  final hasSelection = checkboxStates.values
+                                      .any((isSelected) => isSelected);
+                                  if (!hasSelection) {
+                                    DigitToast.show(
+                                      context,
+                                      options: DigitToastOptions(
+                                        i18_local.deliverIntervention
+                                            .noCheckboxSelect,
+                                        true,
+                                        theme,
+                                      ),
+                                    );
+                                    return;
+                                  }
+
+                                  clickedStatus.value = true;
+
+                                  final task = deliverState.oldTask!.copyWith(
+                                    status:
+                                        Status.administeredSuccess.toValue(),
+                                    additionalFields: TaskAdditionalFields(
+                                      version: deliverState.oldTask!
+                                              .additionalFields?.version ??
+                                          1,
+                                      fields: [
+                                        ...?deliverState
+                                            .oldTask!.additionalFields?.fields,
+                                        ...checkboxStates.entries
+                                            .where(
+                                                (entry) => entry.value == true)
+                                            .map((entry) => AdditionalField(
+                                                  entry.key,
+                                                  Constants.trueString,
+                                                )),
+                                        AdditionalField(
+                                          AdditionalFieldsType
+                                              .dateOfVerification
+                                              .toValue(),
+                                          DateTime.now()
+                                              .millisecondsSinceEpoch
+                                              .toString(),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+
+                                  context.read<DeliverInterventionBloc>().add(
+                                        DeliverInterventionSubmitEvent(
+                                          task: task,
+                                          isEditing: (deliverState.tasks ?? [])
+                                                      .isNotEmpty &&
+                                                  RegistrationDeliverySingleton()
+                                                          .beneficiaryType ==
+                                                      BeneficiaryType.household
+                                              ? true
+                                              : false,
+                                          boundaryModel:
+                                              RegistrationDeliverySingleton()
+                                                  .boundary!,
+                                        ),
+                                      );
+
+                                  final reloadState =
+                                      context.read<HouseholdOverviewBloc>();
+
+                                  reloadState.add(
+                                    HouseholdOverviewReloadEvent(
+                                      projectId: RegistrationDeliverySingleton()
+                                          .projectId!,
+                                      projectBeneficiaryType:
+                                          RegistrationDeliverySingleton()
+                                              .beneficiaryType!,
+                                    ),
+                                  );
+                                  context.router.popAndPush(
+                                    HouseholdAcknowledgementRoute(
+                                      enableViewHousehold: true,
+                                    ),
+                                  );
+                                }
+                              },
+                        child: Center(
+                          child: Text(
+                            localizations
+                                .translate(i18.common.coreCommonSubmit),
                           ),
                         ),
-                      ),
-                      contentPadding: EdgeInsets.zero,
-                      horizontalTitleGap: 8,
-                      minLeadingWidth: 24,
-                      minVerticalPadding: 0,
-                    );
-                  }),
+                      );
+                    },
+                  ),
                 ),
-              ],
-            ),
-          ],
-        ),
+                slivers: [
+                  SliverToBoxAdapter(
+                    child: DigitCard(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextBlock(
+                            padding: const EdgeInsets.only(top: kPadding),
+                            heading: localizations.translate(
+                              i18_local
+                                  .deliverIntervention.mobilityControlLabel,
+                            ),
+                            headingStyle: theme.textTheme.displayMedium,
+                            body: localizations.translate(
+                              i18_local
+                                  .deliverIntervention.mobilityControlQuestion,
+                            ),
+                          ),
+                          ...controlAssessmentQuestions.map((item) {
+                            final checkboxValue = item['value']!;
+                            return ListTile(
+                              title: Text(
+                                checkboxValue,
+                                style: theme.textTheme.bodyLarge,
+                              ),
+                              leading: SizedBox(
+                                width: 24,
+                                height: 24,
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: DigitCheckbox(
+                                    value:
+                                        checkboxStates[checkboxValue] ?? false,
+                                    onChanged: (bool? value) => setState(() =>
+                                        checkboxStates[checkboxValue] =
+                                            value ?? false),
+                                  ),
+                                ),
+                              ),
+                              contentPadding: EdgeInsets.zero,
+                              horizontalTitleGap: 8,
+                              minLeadingWidth: 24,
+                              minVerticalPadding: 0,
+                            );
+                          }).toList(),
+                          const SizedBox(height: 16),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
+  }
+
+  FormGroup buildForm() {
+    return fb.group(<String, Object>{});
   }
 }
