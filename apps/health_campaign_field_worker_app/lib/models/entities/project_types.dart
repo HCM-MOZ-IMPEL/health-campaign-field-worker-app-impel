f945106532843aa24bfac70873bfe0069e3f48e9
amitverma-egov <amit.verma@egovernments.org>
2026-03-06 11:53:41 +0530

    production ready apk changes , removed proximity search

--- apps/health_campaign_field_worker_app/lib/models/entities/project_types.dart
+++ apps/health_campaign_field_worker_app/lib/models/entities/project_types.dart
@@ -7,7 +7,7 @@ part 'project_types.mapper.dart';
 enum ProjectTypes {
   @MappableValue("IRS")
   irs,
-  @MappableValue("SMC-Moz")
+  @MappableValue("SMC-Moz26")
   smc,
   @MappableValue("LLIN-SMC")
   smcAndBednet,
--- apps/health_campaign_field_worker_app/lib/pages/pages-SMC/beneficiary_registration/custom_search_beneficiary_smc.dart
+++ apps/health_campaign_field_worker_app/lib/pages/pages-SMC/beneficiary_registration/custom_search_beneficiary_smc.dart
@@ -143,81 +143,37 @@ class _CustomSearchBeneficiarySMCPageState
                         builder: (context, locationState) {
                           return Column(
                             children: [
-                              locationState.latitude != null
-                                  ? Column(
-                                      children: [
-                                        Row(
-                                          children: [
-                                            Switch(
-                                              value: isProximityEnabled,
-                                              onChanged: (value) {
-                                                searchController.clear();
-                                                setState(() {
-                                                  isProximityEnabled = value;
-                                                  isSearchByBeneficaryIdEnabled =
-                                                      false;
-                                                  lat = locationState.latitude!;
-                                                  long =
-                                                      locationState.longitude!;
-                                                });
-
-                                                if (locationState
-                                                        .hasPermissions &&
-                                                    value &&
-                                                    locationState.latitude !=
-                                                        null &&
-                                                    locationState.longitude !=
-                                                        null &&
-                                                    RegistrationDeliverySingleton()
-                                                            .maxRadius !=
-                                                        null &&
-                                                    isProximityEnabled) {
-                                                  triggerGlobalSearchEvent();
-                                                } else {
-                                                  blocWrapper.clearEvent();
-                                                  triggerGlobalSearchEvent();
-                                                }
-                                              },
-                                            ),
-                                            Text(
-                                              localizations.translate(
-                                                i18.searchBeneficiary
-                                                    .proximityLabel,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                        Row(
-                                          children: [
-                                            Switch(
-                                              value:
-                                                  isSearchByBeneficaryIdEnabled,
-                                              onChanged: (value) {
-                                                searchController.clear();
-                                                context
-                                                    .read<
-                                                        IndividualGlobalSearchSMCBloc>()
-                                                    .add(
-                                                        const SearchHouseholdsSMCEvent
-                                                            .clear());
-                                                setState(() {
-                                                  isSearchByBeneficaryIdEnabled =
-                                                      value;
-                                                  isProximityEnabled = false;
-                                                  searchController.clear();
-                                                  blocWrapper.clearEvent();
-                                                });
-                                              },
-                                            ),
-                                            Text(
-                                              localizations.translate(
-                                                  'SEARCH_BY_BENEFICIARY_ID'),
-                                            ),
-                                          ],
-                                        )
-                                      ],
-                                    )
-                                  : const Offstage(),
+                              Column(
+                                children: [
+                                  Row(
+                                    children: [
+                                      Switch(
+                                        value: isSearchByBeneficaryIdEnabled,
+                                        onChanged: (value) {
+                                          searchController.clear();
+                                          context
+                                              .read<
+                                                  IndividualGlobalSearchSMCBloc>()
+                                              .add(
+                                                  const SearchHouseholdsSMCEvent
+                                                      .clear());
+                                          setState(() {
+                                            isSearchByBeneficaryIdEnabled =
+                                                value;
+                                            isProximityEnabled = false;
+                                            searchController.clear();
+                                            blocWrapper.clearEvent();
+                                          });
+                                        },
+                                      ),
+                                      Text(
+                                        localizations.translate(
+                                            'SEARCH_BY_BENEFICIARY_ID'),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
                               DigitSearchBar(
                                 controller: searchController,
                                 hintText: localizations.translate(
