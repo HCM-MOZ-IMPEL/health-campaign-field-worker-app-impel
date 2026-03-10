649acacebda4112059c5e255dde74e950e3b6872
amitverma-egov <amit.verma@egovernments.org>
2026-03-10 20:04:57 +0530

    revert

--- apps/health_campaign_field_worker_app/lib/pages/pages-SMC/home_smc.dart
+++ apps/health_campaign_field_worker_app/lib/pages/pages-SMC/home_smc.dart
@@ -656,10 +656,13 @@ class HomeSMCPageState extends LocalizedState<HomeSMCPage> {
     ];
 
     final List<String> filteredLabels = homeItemsLabel
-        .where((element) => state.actionsWrapper.actions
-            .map((e) => e.displayName)
-            .toList()
-            .contains(element)) // TODO: need to add close household inside mdms
+        .where((element) =>
+            state.actionsWrapper.actions
+                .map((e) => e.displayName)
+                .toList()
+                .contains(element) ||
+            element ==
+                i18.home.db) // TODO: need to add close household inside mdms
         .toList();
 
     final showcaseKeys = filteredLabels
